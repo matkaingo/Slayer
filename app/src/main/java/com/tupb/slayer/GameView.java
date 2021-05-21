5e5f89d8e238bcf254e6f8f7c52d459e84e9d648
mateo-manjarres <53178459+mateo-manjarres@users.noreply.github.com>
2021-05-21 00:49:57 -0500

    save

---
+++ app/src/main/java/com/tupb/slayer/GameView.java
@@ -0,0 +1,19 @@
+package com.tupb.slayer;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class GameView extends View {
+    public GameView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+    public void  draw(Canvas canvas){
+        super.draw(canvas);
+        canvas.drawColor(Color.BLUE);
+    }
+}
--- app/src/main/java/com/tupb/slayer/MainActivity.java
+++ app/src/main/java/com/tupb/slayer/MainActivity.java
@@ -3,6 +3,7 @@ package com.tupb.slayer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         setTheme(R.style.Theme_Slayer);
         super.onCreate(savedInstanceState);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
     }
 }
