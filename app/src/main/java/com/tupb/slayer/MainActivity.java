762cd14507c28d9ad8877f50338a6ebb67c4ed4f
mateo-manjarres <53178459+mateo-manjarres@users.noreply.github.com>
2021-05-20 22:09:40 -0500

    Create

---
+++ app/src/main/java/com/tupb/slayer/MainActivity.java
@@ -0,0 +1,14 @@
+package com.tupb.slayer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/tupb/slayer/SplashScreen.java
@@ -0,0 +1,9 @@
+package com.tupb.slayer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+
+    
+
+}
