ea3d14553332ec065d6244882bf471c8af3118d0
mateo-manjarres <mateo.manjarres@upb.edu.co>
2021-05-26 09:18:07 -0500

    ac

---
+++ app/src/main/java/com/example/slayer/BaseObject.java
@@ -0,0 +1,59 @@
+package com.example.slayer;
+
+import android.graphics.Bitmap;
+
+public class BaseObject {
+    protected float x, y;
+    private int width, height;
+    protected Bitmap bm;
+
+    public BaseObject() {
+    }
+
+    public BaseObject(float x, float y, int width, int height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public Bitmap getBm() {
+        return bm;
+    }
+
+    public void setBm(Bitmap bm) {
+        this.bm = bm;
+    }
+}
--- app/src/main/java/com/example/slayer/MainActivity.java
+++ app/src/main/java/com/example/slayer/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.slayer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.Display;
 import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,6 +13,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        DisplayMetrics dm = new DisplayMetrics();
+        this.getWindowManager().getDefaultDisplay().getMetrics(dm);
+        Constants.SCREEN_WIDTH = dm.widthPixels;
+        Constants.SCREEN_HEIGHT= dm.heightPixels;
+
         setContentView(R.layout.activity_main);
     }
 }
