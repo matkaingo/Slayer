582df216ab5107f948810646e3f82235918bb107
mateo-manjarres <mateo.manjarres@upb.edu.co>
2021-05-27 17:34:37 -0500

    act

--- app/src/main/java/com/example/slayer/GameView.java
+++ app/src/main/java/com/example/slayer/GameView.java
@@ -1,9 +1,14 @@
 package com.example.slayer;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.media.AudioAttributes;
+import android.media.AudioManager;
+import android.media.SoundPool;
+import android.os.Build;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
@@ -19,10 +24,22 @@ public class GameView extends View {
     private Runnable r;
     private ArrayList<Pipe> arrPipes;
     private int sumpipe, distance;
-    private int score;
+    private int score, bestscore=0;
+    private boolean start;
+    private Context context;
+    private int soundJump;
+    private float volume;
+    private boolean loadedsound;
+    private SoundPool soundPool;
     public GameView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        this.context = context;
+        SharedPreferences sp = context.getSharedPreferences("gamesetting", Context.MODE_PRIVATE);
+        if(sp!=null){
+            bestscore = sp.getInt("bestscore",0);
+        }
         score = 0;
+        start = false;
         initBird();
         initPipe();
         handler = new Handler();
@@ -32,6 +49,25 @@ public class GameView extends View {
                 invalidate();
             }
         };
+        if(Build.VERSION.SDK_INT>=21){
+            AudioAttributes audioAttributes = new AudioAttributes.Builder()
+                    .setUsage(AudioAttributes.USAGE_GAME)
+                    .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+                    .build();
+            SoundPool.Builder builder = new SoundPool.Builder();
+            builder.setAudioAttributes(audioAttributes).setMaxStreams(5);
+            this.soundPool = builder.build();
+        }else{
+            soundPool = new SoundPool(5, AudioManager.STREAM_MUSIC, 0);
+        }
+        this.soundPool.setOnLoadCompleteListener(new SoundPool.OnLoadCompleteListener() {
+            @Override
+            public void onLoadComplete(SoundPool soundPool, int sampleId, int status) {
+                loadedsound = true;
+            }
+        });
+        soundJump = this.soundPool.load(context, R.raw.jump_02,1);
+
     }
 
     private void initPipe() {
@@ -66,33 +102,73 @@ public class GameView extends View {
 
     public void draw(Canvas canvas){
         super.draw(canvas);
-        bird.draw(canvas);
-        for(int i = 0; i<sumpipe; i++){
-            if(this.bird.getX()+this.bird.getWidth()>arrPipes.get(i).getX()+arrPipes.get(i).getWidth()/2
-            && this.bird.getX()+this.bird.getWidth()<=arrPipes.get(i).getX()+arrPipes.get(i).getWidth()/2+Pipe.speed
-            && i<sumpipe/2){
-                score++;
-                MainActivity.txt_score.setText(""+score);
-            }
-             if(this.arrPipes.get(i).getX() < -arrPipes.get(i).getWidth()){
-                this.arrPipes.get(i).setX(Constants.SCREEN_WIDTH);
-                if(i < sumpipe/2){
-                    arrPipes.get(i).randomY();
-                }else {
-                    arrPipes.get(i).setY(this.arrPipes.get(i-sumpipe/2).getY()
-                            +this.arrPipes.get(i-sumpipe/2).getHeight() + this.distance);
+        if(start){
+            bird.draw(canvas);
+            for(int i = 0; i<sumpipe; i++){
+                if (bird.getRect().intersect(arrPipes.get(i).getRect())||bird.getY()-bird.getHeight()<0||bird.getY()>Constants.SCREEN_HEIGHT){
+                    Pipe.speed=0;
+                    MainActivity.txt_score_over.setText(MainActivity.txt_score.getText());
+                    MainActivity.txt_best_score.setText("best: "+bestscore);
+                    MainActivity.txt_score.setVisibility(INVISIBLE);
+                    MainActivity.rl_game_over.setVisibility(VISIBLE);
                 }
-             }
-             this.arrPipes.get(i).draw(canvas);
+                if(this.bird.getX()+this.bird.getWidth()>arrPipes.get(i).getX()+arrPipes.get(i).getWidth()/2
+                        && this.bird.getX()+this.bird.getWidth()<=arrPipes.get(i).getX()+arrPipes.get(i).getWidth()/2+Pipe.speed
+                        && i<sumpipe/2){
+                    score++;
+                    if(score>bestscore){
+                        bestscore = score;
+                        SharedPreferences sp = context.getSharedPreferences("gamesetting", Context.MODE_PRIVATE);
+                        SharedPreferences.Editor editor = sp.edit();
+                        editor.putInt("bestscore", bestscore);
+                        editor.apply();
+                    }
+                    MainActivity.txt_score.setText(""+score);
+                }
+                if(this.arrPipes.get(i).getX() < -arrPipes.get(i).getWidth()){
+                    this.arrPipes.get(i).setX(Constants.SCREEN_WIDTH);
+                    if(i < sumpipe/2){
+                        arrPipes.get(i).randomY();
+                    }else {
+                        arrPipes.get(i).setY(this.arrPipes.get(i-sumpipe/2).getY()
+                                +this.arrPipes.get(i-sumpipe/2).getHeight() + this.distance);
+                    }
+                }
+                this.arrPipes.get(i).draw(canvas);
+            }
+        }else {
+            if(bird.getY()>Constants.SCREEN_HEIGHT/2){
+                bird.setDrop(-15*Constants.SCREEN_HEIGHT/1920);
+            }
+            bird.draw(canvas);
         }
+
         handler.postDelayed(r, 10);
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
         if (event.getAction() == MotionEvent.ACTION_DOWN){
-            bird.setDrop(-15);  
+            bird.setDrop(-15);
+            if(loadedsound){
+                int streamId = this.soundPool.play(this.soundJump, (float)0.5, (float)0.5, 1, 0,1f);
+            }
         }
         return true;
+    }
+
+    public boolean isStart() {
+        return start;
+    }
+
+    public void setStart(boolean start) {
+        this.start = start;
+    }
+
+    public void reset(){
+        MainActivity.txt_score.setText("0");
+        score=0;
+        initPipe();
+        initBird();
     }
 }
